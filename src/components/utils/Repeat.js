826eb3f131af5ce6c044309b78904f3aa9a5bacb
Joshua <j.maxwell.alan@gmail.com>
2021-07-10 19:12:01 -0400

    create repeater component

--- src/components/utils/Repeat.js
+++ src/components/utils/Repeat.js
@@ -1,14 +1,41 @@
 import { cloneElement } from 'react';
 
-export default function Repeat ({ times, children }) {
+export default function Repeat ({ times, children, iterateLabel, iterateStep, reverse }) {
   if (!Array.isArray(children)) children = [children];
-  const keyAwareDeepClone = (El, idx) => cloneElement(
+  const keyAwareDeepClone = (El, idx) => El && cloneElement(
     El,
-    { ...El.props, key: idx },
+    {
+      ...El.props,
+      key: idx
+    },
     El.children && El.children.length && El.children.map(keyAwareDeepClone)
   );
 
   const multiplyElement = el => (new Array(times)).fillByGenerator(idx => keyAwareDeepClone(el, idx));
 
-  return children.map(multiplyElement);
+  const labelElement = (el, idx) => (
+    <RepeatLabel
+      key={idx}
+      label={iterateLabel && idx * iterateStep}
+    >
+      {el}
+    </RepeatLabel>
+  );
+
+  children.mapInPlace(multiplyElement);
+
+  children.mapInPlace(multipliedChild => multipliedChild.map(labelElement));
+
+  if (reverse) children.mapInPlace(multipliedChild => multipliedChild.reverse());
+
+  return children;
+}
+
+function RepeatLabel ({ children, label }) {
+  return (
+    <div className='repeat-label'>
+      {label}
+      {children}
+    </div>
+  );
 }
