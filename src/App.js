603a189ffbf895f064d165e2ceb001182fc8d644
Joshua <j.maxwell.alan@gmail.com>
2021-07-09 23:32:29 -0400

    routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import AudioPlayer from './components/Player';
+import { Route, Switch } from 'react-router-dom';
+import BarChart from './components/BarChart';
+import NavBar from './components/NavBar';
+import PieChart from './components/PieChart';
 
 export default function App () {
   return (
-    <div className='App'>
-      <AudioPlayer />
-    </div>
+    <>
+      <NavBar />
+      <Switch>
+        <Route path='/barchart/'>
+          <BarChart />
+        </Route>
+        <Route path='/piechart/'>
+          <PieChart />
+        </Route>
+      </Switch>
+    </>
   );
 }
