bbd74c980b722146e5ef88b60ed2f494c7b780ad
Joshua <j.maxwell.alan@gmail.com>
2021-07-10 19:11:44 -0400

    create barchart holder

--- src/components/BarChart/index.js
+++ src/components/BarChart/index.js
@@ -1,13 +1,39 @@
+import { cloneElement } from 'react';
 import Repeat from '../utils/Repeat';
 
 import './index.css';
 
-export default function BarChart () {
+export default function BarChart ({ scaleMax, scaleStep, children }) {
+  if (!Array.isArray(children)) children = [children];
+  children = children.map((child, idx) => {
+    return cloneElement(
+      child,
+      {
+        ...child.props,
+        style: {
+          width: `${(100 / children.length) - 1}%`
+        },
+        key: idx
+      },
+      child.children
+    );
+  });
+
   return (
     <div className='bar-box'>
-      <Repeat times={5}>
-        <div className='bar-scale' />
-      </Repeat>
+      <div className='bar-scale-container'>
+        <Repeat
+          iterateLabel
+          reverse
+          times={Math.round(((scaleMax / scaleStep) * 100 + Number.EPSILON) / 100) + 1}
+          iterateStep={scaleStep}
+        >
+          <div className='bar-scale' />
+        </Repeat>
+      </div>
+      <div className='bar-container'>
+        {children}
+      </div>
     </div>
   );
 }
