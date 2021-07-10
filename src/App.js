db0538096acff28d501c2959abd7a0b9194df2d8
Joshua <j.maxwell.alan@gmail.com>
2021-07-10 19:11:03 -0400

    wrap barchart in own page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Route, Switch } from 'react-router-dom';
-import BarChart from './components/BarChart';
+import BarPage from './components/BarPage';
 import NavBar from './components/NavBar';
 import PieChart from './components/PieChart';
 
@@ -7,14 +7,16 @@ export default function App () {
   return (
     <>
       <NavBar />
-      <Switch>
-        <Route path='/barchart/'>
-          <BarChart />
-        </Route>
-        <Route path='/piechart/'>
-          <PieChart />
-        </Route>
-      </Switch>
+      <div id='below-navbar'>
+        <Switch>
+          <Route path='/barchart/'>
+            <BarPage />
+          </Route>
+          <Route path='/piechart/'>
+            <PieChart />
+          </Route>
+        </Switch>
+      </div>
     </>
   );
 }
